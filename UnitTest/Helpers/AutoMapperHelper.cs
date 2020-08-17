48fbbd801c356ba200a2bb38a958f28c08eec6b1
Maks98791 <maksim.birszel@gmail.com>
2020-08-17 19:38:06 +0200

    Add automapper helper for tests project

---
+++ UnitTest/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestCreatorWebApp.Mapping;
+
+namespace UnitTests.Helpers
+{
+    class AutoMapperHelper
+    {
+        private static IMapper _mapper;
+
+        public static IMapper Mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                {
+                    // Auto Mapper Configurations
+                    var mappingConfig = new MapperConfiguration(mc =>
+                    {
+                        mc.AddProfile(new TestCreatorWebApp.Mapping.Mapper());
+                    });
+                    IMapper mapper = mappingConfig.CreateMapper();
+                    _mapper = mapper;
+                }
+
+                return _mapper;
+            }
+        }
+    }
+}
